6f4f7cb9ddd268ccd8a92bef5a9f95f637363401
Solm1504 <solm1504@gmail.com>
2023-04-06 14:46:52 +0000

    Shineed

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+  const handleAddTodo = (event) => {
+    event.preventDefault();
+    if (newTodo !== "") {
+      setTodos([...todos, newTodo]);
+      setNewTodo("");
+    }
+  };
 
-export default Home;
+  const handleDeleteTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="container">
+      <h1 className="text-center my-4">To do List</h1>
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <form onSubmit={handleAddTodo}>
+            <div className="input-group mb-3">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Add a new task"
+                aria-label="Add a new task"
+                aria-describedby="add-button"
+                value={newTodo}
+                onChange={(event) => setNewTodo(event.target.value)}
+              />
+              <button
+                className="btn btn-outline-secondary"
+                type="submit"
+                id="add-button"
+              >
+                Add
+              </button>
+            </div>
+          </form>
+          <ul className="list-group">
+            {todos.length > 0 ? (
+              todos.map((todo, index) => (
+                <li className="list-group-item d-flex justify-content-between align-items-center">
+                  {todo}
+                  <button
+                    className="btn btn-sm btn-danger"
+                    onClick={() => handleDeleteTodo(index)}
+                  >
+                    X
+                  </button>
+                </li>
+              ))
+            ) : (
+              <li className="list-group-item text-muted">
+               No tasks, add a task
+              </li>
+            )}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TodoList;
+
